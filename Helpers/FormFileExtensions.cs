43032acacc9b59595d2ea2331fe87aea4d5f4a4a
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2026-09-20 23:55:26 +0300

    add helpers

---
+++ Helpers/FormFileExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace IntelligentDocAnalyzer.Helpers;
+
+public static class FormFileExtensions
+{
+    private static readonly HashSet<string> AllowedExtensions =
+        new(StringComparer.OrdinalIgnoreCase)
+        {
+            ".pdf",
+            ".png",
+            ".jpg",
+            ".jpeg",
+            ".tiff"
+        };
+
+    public static bool HasAllowedExtension(this IFormFile file)
+    {
+        if (file == null)
+            return false;
+
+        var extension = Path.GetExtension(file.FileName);
+
+        return AllowedExtensions.Contains(extension);
+    }
+}
---
+++ Helpers/StreamExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace IntelligentDocAnalyzer.Helpers;
+
+public static class StreamExtensions
+{
+    public static async Task<MemoryStream> ToMemoryStreamAsync(this IFormFile file, CancellationToken cancellationToken = default)
+    {
+        var ms = new MemoryStream();
+
+        await file.CopyToAsync(ms, cancellationToken);
+        ms.Position = 0;
+
+        return ms;
+    }
+}
