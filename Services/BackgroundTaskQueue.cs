8e4146bb632bb4f3536b69d3dc68a84d8182ab23
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2026-09-23 15:09:34 +0300

    add task queue

---
+++ Services/BackgroundTaskQueue.cs
@@ -0,0 +1,20 @@
+﻿
+using IntelligentDocAnalyzer.Interfaces;
+using System.Threading.Channels;
+
+namespace IntelligentDocAnalyzer.Services;
+
+public class BackgroundTaskQueue : IBackgroundTaskQueue
+{
+    private readonly Channel<Guid> _queue = Channel.CreateUnbounded<Guid>(new UnboundedChannelOptions { SingleReader = true, SingleWriter = false });
+
+    public async ValueTask EnqueueAsync(Guid jobId, CancellationToken cancellationToken = default)
+    {
+        await _queue.Writer.WriteAsync(jobId, cancellationToken);
+    }
+
+    public async ValueTask<Guid> DequeueAsync(CancellationToken cancellationToken)
+    {
+        return await _queue.Reader.ReadAsync(cancellationToken);
+    }
+}
