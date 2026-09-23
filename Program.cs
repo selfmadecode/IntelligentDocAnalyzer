f3b4c3fe9d5a22a549c9674732c040193417160b
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2026-09-23 15:59:23 +0300

    register DI services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using IntelligentDocAnalyzer.Interfaces;
 using IntelligentDocAnalyzer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,8 +11,11 @@ builder.Services.AddSwaggerGen();
 // Register Azure Document Intelligence client as a singleton factory
 builder.Services.AddSingleton<DocumentIntelligenceService>();
 // Processing and analysis services
-builder.Services.AddScoped<StatementProcessor>();
-builder.Services.AddScoped<FinancialAnalyzer>();
+builder.Services.AddSingleton<StatementProcessor>();
+builder.Services.AddSingleton<FinancialAnalyzer>();
+builder.Services.AddSingleton<IJobStore, InMemoryJobStore>();
+builder.Services.AddSingleton<IBackgroundTaskQueue, BackgroundTaskQueue>();
+builder.Services.AddHostedService<ThrottledRecognitionWorker>();
 
 var app = builder.Build();
 
