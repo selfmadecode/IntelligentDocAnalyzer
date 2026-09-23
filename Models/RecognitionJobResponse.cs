dc85047a37874924dafd700af0be08ccc51e0a25
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2026-09-23 15:09:20 +0300

    add job response

---
+++ Models/RecognitionJobResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace IntelligentDocAnalyzer.Models;
+
+public class RecognitionJobResponse
+{
+    public Guid Id { get; }
+    public string Status { get; }
+    public string FileName { get; }
+    public DateTime SubmittedAtUtc { get; }
+    public DateTime? CompletedAtUtc { get; }
+    public string? ModelUsed { get; }
+    public List<Transaction>? Result { get; }
+    public string? ErrorMessage { get; }
+
+    public RecognitionJobResponse(RecognitionJob job)
+    {
+        Id = job.Id;
+        Status = job.Status.ToString();
+        FileName = job.FileName;
+        SubmittedAtUtc = job.SubmittedAtUtc;
+        CompletedAtUtc = job.CompletedAtUtc;
+        ModelUsed = job.ModelUsed;
+        Result = job.Status == JobStatus.Completed ? job.Result : null;
+        ErrorMessage = job.ErrorMessage;
+    }
+}
