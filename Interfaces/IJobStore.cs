aac579fb7395ccb17543b5011bbafba2e7e10964
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2026-09-23 15:01:38 +0300

    add job store

---
+++ Interfaces/IJobStore.cs
@@ -0,0 +1,10 @@
+﻿using IntelligentDocAnalyzer.Models;
+
+namespace IntelligentDocAnalyzer.Interfaces;
+
+public interface IJobStore
+{
+    RecognitionJob Create(string fileName, BinaryData content);
+    bool TryGet(Guid id, out RecognitionJob? job);
+    void Update(RecognitionJob job);
+}
